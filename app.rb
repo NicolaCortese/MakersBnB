4d55a47c6267ac4e6af583b560561ba4a828d6b7
Nicola Cortese <nicolacortese92@gmail.com>
2022-03-22 14:32:29 +0000

    changing listing to space

--- app.rb
+++ app.rb
@@ -53,6 +53,17 @@ class Makersbnb < Sinatra::Base
       redirect  '/login'
     end
   end
+  
+  post '/booking/:id' do
+    @booking = Booking.create(
+      user_id: session[:user_id], 
+      space_id: params[:id]
+    )
+    @booking.save
+      
+    flash[:notice]= "Booking successfull!"
+    redirect '/'
+  end
 
   run! if app_file == $PROGRAM_NAME
 end
