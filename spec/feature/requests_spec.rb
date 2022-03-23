c2108444d27acccc37d48a3b6981cfc62870de4d
clovellbsc <clovellbsc@gmail.com>
2022-03-23 17:06:52 +0000

    Added buttons for accepting and declining requests made and associated test

--- spec/feature/requests_spec.rb
+++ spec/feature/requests_spec.rb
@@ -2,7 +2,7 @@
 
 feature 'Requests' do
   scenario 'Viewing requests' do
-    sign_up
+    sign_up_bobby
     click_button 'Requests'
 
     expect(current_path).to eq '/requests'
@@ -11,10 +11,10 @@ feature 'Requests' do
   end
 
   scenario 'Viewing all requests made' do
-    sign_up
-    create_space
+    sign_up_bobby
+    create_space_damp_cave
     click_button 'Log out'
-    alternate_sign_up
+    sign_up_babdul
     click_button 'View Listing' # there is only one so we don't have to specify which button
     click_button 'Request to book'
     click_button 'Requests'
@@ -25,10 +25,10 @@ feature 'Requests' do
   end
 
   scenario 'Viewing all requests received' do
-    sign_up
-    create_space
+    sign_up_bobby
+    create_space_damp_cave
     click_button 'Log out'
-    alternate_sign_up
+    sign_up_babdul
     click_button 'View Listing' 
     click_button 'Request to book'
     click_button 'Log out'
@@ -40,5 +40,39 @@ feature 'Requests' do
 
     expect(page).to have_content 'A Damp Cave'
     expect(page.text.index("A Damp Cave")).to be > page.text.index("Requests Received")
+    expect(page).to have_content 'Requested by Bab'
   end
+
+  scenario 'Viewing the status of my request as pending' do
+    sign_up_bobby
+    create_space_damp_cave
+    click_button 'Log out'
+    sign_up_babdul
+    click_button 'View Listing' 
+    click_button 'Request to book'
+    click_button 'Requests'
+
+    expect(page).to have_content 'A Damp Cave'
+    expect(page).to have_content 'Status Pending'
+  end
+
+  scenario "Able to accept or decline requests to book my property" do
+    sign_up_bobby
+    create_space_damp_cave
+    click_button 'Log out'
+    sign_up_babdul
+    click_button 'View Listing' 
+    click_button 'Request to book'
+    click_button 'Log out'
+    click_button 'Login'
+    fill_in 'username', with: 'Bob'
+    fill_in 'password', with: 'Bob1'
+    click_button 'Log in'
+    click_button 'Requests'
+
+    expect(page).to have_button 'Accept'
+    expect(page).to have_button 'Decline'
+  end
+
+
 end
