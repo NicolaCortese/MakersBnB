0ad918a105d3d98b777f8f8db7dbe306856c78c4
clovellbsc <clovellbsc@gmail.com>
2022-03-23 15:23:44 +0000

    updated db/migrations bookings tables

--- db/migrations/04_creating_bookings_table.sql
+++ db/migrations/04_creating_bookings_table.sql
@@ -1,4 +1,4 @@
 \c makersbnb
-CREATE TABLE bookings(id SERIAL PRIMARY KEY, space_id int, user_id int, booked_from VARCHAR(60), booked_to VARCHAR(60));
+CREATE TABLE bookings(id SERIAL PRIMARY KEY, space_id int, user_id int, booked_from VARCHAR(60), booked_to VARCHAR(60), created_at TIMESTAMP, accepted? BOOLEAN);
 \c makersbnb_test
-CREATE TABLE bookings(id SERIAL PRIMARY KEY, space_id int, user_id int, booked_from VARCHAR(60), booked_to VARCHAR(60));
+CREATE TABLE bookings(id SERIAL PRIMARY KEY, space_id int, user_id int, booked_from VARCHAR(60), booked_to VARCHAR(60), created_at TIMESTAMP, accepted? BOOLEAN);
